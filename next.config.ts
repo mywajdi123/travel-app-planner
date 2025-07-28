923cd702acd5cd5310ca0be6a07c52811595df02
Mohamad Yaqoob Wajdi <yaqoob.wajdi@gmail.com>
2025-07-28 15:55:14 -0400

    app setup

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "ovco9b5jyh.ufs.sh",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
