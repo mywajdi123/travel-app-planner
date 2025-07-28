4bb2b27b2e8abece0acb46366387a42d703561f0
Mohamad Yaqoob Wajdi <yaqoob.wajdi@gmail.com>
2025-07-28 18:51:45 -0400

    files for primsa

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"DIW9fkYUm/voPdV02/kpuULFYzQMb2ztdRaMFuZomEo=\"\n}"
--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        hostname: "ovco9b5jyh.ufs.sh",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
